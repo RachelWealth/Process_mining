9e96861edc931fa3f880f80edac6b5c056bc16d6
RachelWealth <74123687+RachelWealth@users.noreply.github.com>
2023-09-12 10:55:41 +0200

    exercise 2

---
+++ exercise2.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time : 11/09/2023 23:49
+# @Author :Rachel Duan
+# @Email : rachel_duanyingli@163.com
+# @File : exercise2.py
+# @Project : pycharm
--- main.py
+++ main.py
@@ -3,21 +3,33 @@ from excersice1 import PetriNet
 
 p = PetriNet()
 
-p.add_place(1)  # add place with id 1
+p.add_place(1)
+
 p.add_place(2)
+
 p.add_place(3)
+
 p.add_place(4)
-p.add_transition("A", -1)  # add transition "A" with id -1
+
+p.add_place(5)
+
+p.add_place(6)
+
+p.add_transition("A", -1)
+
 p.add_transition("B", -2)
+
 p.add_transition("C", -3)
+
 p.add_transition("D", -4)
 
 p.add_edge(1, -1)
-p.add_edge(-1, 2)
-p.add_edge(2, -2).add_edge(-2, 3)
-p.add_edge(2, -3).add_edge(-3, 3)
-p.add_edge(3, -4)
-p.add_edge(-4, 4)
+
+p.add_edge(-1, 2).add_edge(2, -2).add_edge(-2, 4).add_edge(4, -4)
+
+p.add_edge(-1, 3).add_edge(3, -3).add_edge(-3, 5).add_edge(5, -4)
+
+p.add_edge(-4, 6)
 
 print(p.is_enabled(-1), p.is_enabled(-2), p.is_enabled(-3), p.is_enabled(-4))
 
