1c541afe146049fc289944d9e3b6f1fc6f43fab5
Yingli Duan <s222462@dtu.dk>
2023-09-12 11:20:09 +0200

    update exercise 2

--- exercise2.py
+++ exercise2.py
@@ -4,3 +4,11 @@
 # @Email : rachel_duanyingli@163.com
 # @File : exercise2.py
 # @Project : pycharm
+def log_as_dictionary(log):
+    dic_log={}
+    events=log.split("\n")
+    for event in events:
+        fields=event.spil(";")
+        if fields[1] not in dic_log.keys():
+            dic_log[fiels[1]]=[]
+        else:
