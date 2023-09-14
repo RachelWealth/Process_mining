4727fc6a55e06c9884340484bcba3eb4df140d9b
Yingli Duan <s222462@dtu.dk>
2023-09-14 16:58:21 +0200

    X_L

--- ex3.py
+++ ex3.py
@@ -13,15 +13,16 @@ T_I = set({})   # The set of start activities
 T_O = set({})   # The set of end activities
 CAUSAL=set() # The set of all causal relationgship elements
 CHOICE=set()
-X_L = set({})
+X_L = set((set(),set()))
 EVENTS=set()
-EVENTS_PER=set()
+EVENTS_PRE=set()
 
-def define_all_event_permutation():
+def define_all_event_permutation(log):
     for i in range(len(EVENTS)):
         if i == len(case.keys()) - 1:
             continue
         else:
+            EVENTS_PRE.add((case[i]["concept:name"],case[i+1]["concept:name"]))
             
         
 def casual_per_case(case):
@@ -30,18 +31,55 @@ def casual_per_case(case):
     for i in range(len(case.keys())):
         if i == len(case.keys()) - 1:
             continue
-        ei = case[i]
+        ei = case[i]["concept:name"]
         for j in range(1, len(case.keys())):
-            ej = case[j]
+            ej = case[j]["concept:name"]
             if (ej, ei) not in CAUSAL:
                 global CAUSAL.add((ei, ej))
             else:
                 global CAUSAL.remove((ej, ei))
 def choice_per_case(case):
+    global CHOICE = EVENTS_PRE
     for i in range(len(case)):
         if i == len(case.keys()) - 1:
             continue
+        CHOICE.remove((case[i]["concept:name"],case[j]["concept:name"]))
+        CHOICE.remove((case[j]["concept:name"],case[i]["concept:name"]))
 
+def X_L():
+    A=set()
+    B=set()
+    ADD0=True
+    ADD1=True
+    global CHOICE
+    global CAUSAL
+    global EVENTS
+    # initial
+    X_L.add(((list(CHOICE)[0]),(list(CHOICE)[1])))
+    print("first X_L is:", X_L)
+    for ei in EVENTS:
+        # iterate all the sets n X_L, check whether they 
+        for x in X_L:# (set(),set())
+            if ei not in x[0]:
+                for ax in x[0]:
+                    if (ax,ei) not in CHOICE:
+                        ADD0=False
+                    if(ax,ei) not in CAUSAL:
+                        ADD1=False
+                for bx in x[1]:
+                    if (ei,bx) not in CAUSAL:
+                        ADD0=False
+                    if (ei,bx) not in CHOICE:
+                        ADD1=False
+                if ADD0:
+                    X_L.add((x[0].add(ei),x[1]))
+                if ADD:
+                    X_L.add((x[0],x[1].add(ei)))
+              
+                    
+      
+    
+    
 def alpha(log):
     # log: {"case":{"event1"{},"event2"{}}}
     for case in log.keys():
@@ -83,6 +121,7 @@ def read_from_file(filename):
             case[event_no] = e
             event_no += 1
         log[key] = case
+    define_all_event_permutation(log)
     return log
 
 def transition_name_to_id():
